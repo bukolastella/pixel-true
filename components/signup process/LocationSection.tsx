12cb155cdb2b831ff6d0d30293c5568bbd007cf4
bukolastella <abolarinbukolastella@gmail.com>
2024-11-20 22:16:05 +0100

    added google maps

--- components/signup process/LocationSection.tsx
+++ components/signup process/LocationSection.tsx
@@ -3,6 +3,7 @@ import React, { FC } from "react";
 import GreenCustomBtn from "../ui/GreenCustomBtn";
 import { ThemedText } from "../ThemedText";
 import { Colors } from "@/constants/Colors";
+import MapView, { Marker } from "react-native-maps";
 
 interface Props {
   onNext: () => void;
@@ -10,11 +11,28 @@ interface Props {
 
 const LocationSection: FC<Props> = ({ onNext }) => {
   const theme = useColorScheme() ?? "light";
+  const region = {
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
 
   return (
     <>
+      <MapView style={styles.map} initialRegion={region}>
+        <Marker
+          draggable
+          coordinate={{
+            latitude: region.latitude,
+            longitude: region.longitude,
+          }}
+          // onDragEnd={(e) => console.log(e.nativeEvent.coordinate)}
+        />
+      </MapView>
       <Pressable
         style={[styles.btn, { backgroundColor: Colors[theme].inputBg }]}
+        onPress={onNext}
       >
         <ThemedText type="book">Set Location</ThemedText>
       </Pressable>
@@ -40,5 +58,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 15,
+  },
+  map: {
+    width: "100%",
+    height: 200,
   },
 });
